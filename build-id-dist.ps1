2a1ea76a4dfdc5bb51ba5a5f47f145654e29a89f
Nickolas Gupton <nickolas@gupton.xyz>
2022-07-29 15:23:51 -0400

    Add windows compatible build commands

--- build-id-dist.ps1
+++ build-id-dist.ps1
@@ -1,13 +1,13 @@
 git clone --depth 1 --branch v2.21.1 https://github.com/openstreetmap/iD
 
-rm -rf dist/
-rm -rf out/
+Remove-Item dist/ -Recurse
+Remove-Item out/ -Recurse
 
 cd iD
 npm install
 npm run all
 
-mv ./dist ../dist
+Move-Item ./dist ../dist
 
 cd ..
-rm -rf ./iD
+Remove-Item ./iD -Recurse

a162c1ea41fe8c4b8dc752d8e87da3ac416c212e
Nickolas Gupton <nickolas@gupton.xyz>
2022-07-29 10:06:23 -0400

    Add build definitions for win, mac, and linux. Start working on url handler

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 const { app, BrowserWindow, shell } = require('electron');
 const path = require('path');
 
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+}
+
 function createWindow () {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
@@ -18,25 +24,46 @@ function createWindow () {
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
-    mainWindow.webContents.on('new-window', function(e, url) {
+
+    // Open external links in the users browser
+    mainWindow.webContents.setWindowOpenHandler(function(e, url) {
         e.preventDefault();
         shell.openExternal(url);
     });
 }
 
+app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) {
+            mainWindow.restore();
+        }
+
+        mainWindow.focus();
+    }
+});
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-  });
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+// Handle the protocol. In this case, we choose to show an Error Box.
+app.on('open-url', (event, url) => {
+    console.log('Arrived from: ' + url);
+
+    // TODO: fix this to actually handle the URL properly
+    mainWindow.loadFile('dist/index.html')
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -50,3 +77,11 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+let isDefault = app.isDefaultProtocolClient('id-electron');
+
+if (!isDefault) {
+    console.log('Not the default handler for id-electron:// links, adding...');
+
+    let handlerRegistered = app.setAsDefaultProtocolClient('id-electron');
+    console.log('Handler registered successfully? ' + handlerRegistered);
+}
